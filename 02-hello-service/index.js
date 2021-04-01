ba8d1acbe036692c0bbd200e55aba5aea6862789
hliyan <hasithaliyan@gmail.com>
2021-04-01 17:50:55 +0530

    sending headers

--- 02-hello-service/index.js
+++ 02-hello-service/index.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  // send a response
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
   res.write('Hello world\n');
   res.end();
 });
